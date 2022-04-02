96c3cc026c0cea77c753998294c311c80657b743
Maxwell Wang <maxwang@udel.edu>
2022-04-02 19:36:59 -0400

    added the unoplayer file

---
+++ UnoPlayer.hpp
@@ -0,0 +1,16 @@
+#ifndef UNOPLAYER_HPP_
+#define UNOPLAYER_HPP_
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+
+class UnoPlayer {
+	string playerName;
+public:
+	UnoPlayer(string playerName);
+	virtual ~UnoPlayer();
+};
+
+#endif /* UNOPLAYER_HPP_ */
