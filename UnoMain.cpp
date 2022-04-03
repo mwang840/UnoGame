8980a87eee2a7bcc95b7f739824b7aaf60300cac
Maxwell Wang <maxwang@udel.edu>
2022-04-02 21:08:41 -0400

    main function in uno

---
+++ UnoMain.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "Uno.hpp"
+#include "UnoDeck.hpp"
+#include "UnoPlayer.hpp"
+#include <math.h>
+using namespace std;
+
+int main(){
+	int players = 0;
+	bool uno = false;
+	bool unoOut = false;
+	bool oneLeft = false;
+	cout<<"# of players has to be two or more" <<endl;
+	cin << players;
+	if(players < 2){
+		cout<<"Try again"<<endl;
+	}
+	else{
+		//starts the game
+		UnoDeck *hand = new UnoDeck();
+		hand->deal();
+
+		//if(hand->player->)
+
+		if(unoOut == true){
+			cout<<hand->player << " is out of the game!"<<endl;
+			delete hand;
+			players--;
+
+			if(players == 1){
+				oneLeft = true;
+			}
+
+			if(oneLeft == true){
+				cout<<"Game over, you lost"<<endl;
+			}
+		}
+
+
+
+	}
+
+
+	return 0;
+}
