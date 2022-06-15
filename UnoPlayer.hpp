5ce7a9f010798557e053d6d66a0af1246d10d172
mwang840 <maxwang@udel.edu>
2022-06-14 22:00:57 -0400

    added player attributes

--- UnoPlayer.hpp
+++ UnoPlayer.hpp
@@ -2,11 +2,18 @@
 #define UNOPLAYER_HPP_
 #include <iostream>
 #include <time.h>
+#include "UnoDeck.hpp"
 
 using namespace std;
 
 
 class UnoPlayer {
+	friend class UnoDeck;
+	UnoPlayer *prev;
+	UnoPlayer *next;
+	UnoPlayer *hand;
+	UnoPlayer *last;
+	UnoPlayer *tail;
 	string playerName;
 public:
 	UnoPlayer(string playerName);
